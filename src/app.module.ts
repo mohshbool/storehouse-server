1087e51dd4a4f02ab864923f90ac1faf19772bc1
Mohammad Shbool <alshboolmoh@gmail.com>
2021-12-13 14:27:41 +0200

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/storehouse', {
+      connectionFactory: (connection) => {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        connection.plugin(require('mongoose-autopopulate'));
+        return connection;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
